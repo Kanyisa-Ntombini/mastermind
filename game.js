0b32755d6913dccdcfb50a21cbabc5e48cadcb0b
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2022-01-08 19:46:58 +0200

    added a js file

---
+++ game.js
@@ -0,0 +1,13 @@
+console.log('Hello world /n');
+
+var readline = require('readline');
+
+var rl = readline.createInterface(
+		process.stdin, process.stdout);
+
+rl.setPrompt(`What is your age? `);
+rl.prompt();
+rl.on('line', (age) => {
+	console.log(`Age received by the user: ${age}`);
+	rl.close();
+});
